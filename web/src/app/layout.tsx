ffaeb5976d0a1551f70adc536f091657de135faf
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-05-16 20:49:15 -0300

    config: setup next google fonts

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,9 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Roboto_Flex as Roboto, Bai_Jamjuree as BaiJamjuree } from 'next/font/google'
 import { ReactNode } from 'react'
-const inter = Inter({ subsets: ['latin'] })
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamjuree = BaiJamjuree({ subsets: ['latin'], weight: '700', variable: '--font-bai-jamjuree' })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans`}>{children}</body>
     </html>
   )
 }
