8276902a2e8a355708933d91e4ecef8c1bd3fc22
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-05-17 19:20:24 -0300

    feat: implement server CRUD requests with prisma

---
+++ server/src/routes/memories.ts
@@ -0,0 +1,105 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get('/memories', async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        // retorna apenas um quantidade específica de caracteres e add reticências
+        excerpt: memory.content.substring(0, 115).concat('...'),
+      }
+    })
+  })
+
+  app.get('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    return memory
+  })
+
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      // utilizar coerce pois exsite uma dificuldade em mandar um booleano durante uma requisição
+      // ex: form de checkbox, ele resulta em resultados de valores comparados a false (0, undefined, '', null) e a true (1, 'fdufhud')
+      // o coerce converte esses resultados para true or false
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: '5ac8562f-5c67-49da-8319-5646cf8b8b6d',
+      },
+    })
+
+    return memory
+  })
+
+  app.put('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+      },
+    })
+
+    return memory
+  })
+
+  app.delete('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.memory.delete({
+      where: {
+        id,
+      },
+    })
+  })
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,37 @@
+// import fastify from 'fastify'
+// import { PrismaClient } from '@prisma/client'
+
+// const app = fastify()
+// const prisma = new PrismaClient()
+
+// app.get('/hello', async () => {
+//   const users = await prisma.user.findMany()
+//   return users
+// })
+
+// app
+//   .listen({
+//     port: 3333,
+//   })
+//   .then(() => {
+//     console.log('HTTP Server is running on http://localhost:3333')
+//   })
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/hello', async () => {
-  const users = await prisma.user.findMany()
-  return users
+app.register(cors, {
+  origin: true, // todas as URLS de front-end poderão acessar nosso backend
 })
+
+app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('HTTP Server is running on http://localhost:3333')
+    console.log('🚀 HTTP server running on port http://localhost:3333')
   })
