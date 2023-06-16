4ba330aeacaec3d34aba744bbc07cd141cf61256
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-06-15 21:30:52 -0300

    feat: creat mobile memories listing

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,19 +1,45 @@
 import { Link, useRouter } from 'expo-router'
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import Icon from '@expo/vector-icons/Feather'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import * as SecureStore from 'expo-secure-store'
+import { useEffect, useState } from 'react'
+import { api } from '../src/lib/api'
+import dayjs from 'dayjs'
+import ptBR from 'dayjs/locale/pt-br'
+dayjs.locale(ptBR)
+
+interface Memory {
+  coverUrl: string
+  excerpt: string
+  id: string
+  createdAt: string
+}
 
 export default function Memories() {
   const router = useRouter()
   const { top, bottom } = useSafeAreaInsets()
+  const [memories, setMemories] = useState<Memory[]>([])
 
   const signOut = async () => {
     await SecureStore.deleteItemAsync('token')
     router.push('/')
   }
 
+  const loadMemories = async () => {
+    const token = await SecureStore.getItemAsync('token')
+    const response = await api.get('/memories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    setMemories(response.data)
+  }
+
+  useEffect(() => {
+    loadMemories()
+  }, [])
 
   return (
     <ScrollView className="flex-1 px-8" contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}>
@@ -36,6 +62,40 @@ export default function Memories() {
         </View>
       </View>
 
+      <View className="mt-6 space-y-10">
+        {memories.map((memory) => {
+          return (
+            <View key={memory.id} className="space-y-4">
+              <View className="flex-row items-center gap-2">
+                <View className="h-px w-5 bg-gray-50 " />
+                <Text className="font-body text-sm text-gray-100">
+                  {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
+                </Text>
+              </View>
+              <View className="space-y-4 px-8">
+                <Image
+                  className="aspect-video w-full rounded-lg"
+                  alt=""
+                  source={{
+                    uri: memory.coverUrl,
+                  }}
+                />
+                <Text className="font-body text-base leading-relaxed text-gray-100">
+                  {memory.excerpt}
+                </Text>
+                <Link href="/memories/id" asChild>
+                  <TouchableOpacity className="flex-row items-center gap-2">
+                    <Text className="font-body text-sm text-gray-200">
+                      Ler mais
+                    </Text>
+                    <Icon name="arrow-right" size={16} color="#9e9ea0" />
+                  </TouchableOpacity>
+                </Link>
+              </View>
+            </View>
+          )
+        })}
+      </View>
     </ScrollView >
   )
 }
